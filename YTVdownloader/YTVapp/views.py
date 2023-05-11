35fec2dc884023e23a3fb18755ead41db3400861
Akanksha <akanksha.sharma173451@gmail.com>
2023-05-11 13:38:09 +0530

    YT Video downloader

---
+++ YTVdownloader/YTVapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from pytube import YouTube
+
+# Create your views here.
+
+def index(request):
+    try :
+        #check request.method is post or not
+        if request.method == 'POST':
+            try :
+                # get link from html form
+                link = request.POST['link']
+                video = YouTube(link)
+                
+                #set video resolution
+                stream = video.streams.get_lowest_resolution()
+                
+                #download the video
+                stream.download()
+                
+                #render HTML page
+                return render(request, 'index.html', {'msg': 'Video downloaded'})
+            except:
+                return render(request, 'index.html', {'msg': 'Video not downloaded'})
+        return render(request, 'index.html', {'msg': ''})
+    except:
+        return render(request, "index.html", {"msg": "Sorry something went wrong ! "})
+                
+    
